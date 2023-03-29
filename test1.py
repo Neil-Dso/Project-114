fe084c73c417a9864a288d17c970ff5480efde99
Neil-Dso <121952365+Neil-Dso@users.noreply.github.com>
2023-03-29 20:32:35 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+a = 33
+
+# For 'b' 33 shouldn't have double quotes as it is an integer
+b = 33
+
+if b > a:
+    print('b is greater than a.')
+elif b == a:
+    print('b is equal to a.')
+
+# If 33 was in double quotes, an error would be thrown
---
+++ test2.py
@@ -0,0 +1,9 @@
+def my_function (fname):
+    print(fname + "\t Refsnes")
+
+#The names must be enclosed within quotes
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+# Otherwise an error will be thrown
